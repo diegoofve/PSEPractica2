26ec07df3c3391ba318b5d4936c7c80ccd8b4e2e
Raul <raul.fernandez23@estudiantes.uva.es>
2026-03-12 17:20:11 +0100

    index

---
+++ servidor/index.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import cors from 'cors';
+import type { Request, Response } from 'express';
+//import{ prisma } from './lib/db';
+
+const PORT = 3000;
+
+//Instanciamos express
+const app = express();
+
+//JSON
+app.use(express.json());
+app.use(cors());
+
+//Definimos la ruta raíz (/) que devolverá una respuesta basada en texto
+app.get('/', (req: Request, res: Response) => {
+    res.send(`Soy el servidor respondiendo`);
+})
+
