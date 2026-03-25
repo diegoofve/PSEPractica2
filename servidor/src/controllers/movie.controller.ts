6d4a39570b0ca491c4381c814c1394e6f036a6ee
Raúl <raul.fernandez23@estudiantes.uva.es>
2026-03-25 18:22:28 +0100

    fix prisma connect duplicado

--- servidor/src/controllers/movie.controller.ts
+++ servidor/src/controllers/movie.controller.ts
@@ -1,8 +1,6 @@
 import { Request, Response } from 'express';
-import { PrismaClient } from '@prisma/client';
+import { prisma } from '../lib/db';
 import { MovieFilterSchema } from '../dtos/movie.dto';
-
-const prisma = new PrismaClient();
 
 export const getMovies = async (req: Request, res: Response): Promise<void> => {
   try {
