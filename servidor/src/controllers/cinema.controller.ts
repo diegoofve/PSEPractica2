0b1bb9ffb79aad92c4b1f6aeabaf229fe7e04f39
Diego <151045577+diegoofve@users.noreply.github.com>
2026-03-25 18:05:35 +0100

    cambiar y terminar el labo 03

--- servidor/src/controllers/cinema.controller.ts
+++ servidor/src/controllers/cinema.controller.ts
@@ -1,12 +1,13 @@
 import { Request, Response } from 'express';
 import { PrismaClient } from '@prisma/client';
-import { MovieFilterSchema } from '../dtos/movie.dto';
+import { CinemaFilterSchema } from '../dtos/cinema.dto';
 
 const prisma = new PrismaClient();
 
-export const getMovies = async (req: Request, res: Response): Promise<void> => {
+export const getCinemas = async (req: Request, res: Response): Promise<void> => {
   try {
-    const validation = MovieFilterSchema.safeParse(req.body);
+    // 1. Validar body
+    const validation = CinemaFilterSchema.safeParse(req.body);
 
     if (!validation.success) {
        res.status(400).json({ errors: validation.error.issues });
@@ -14,21 +15,20 @@ export const getMovies = async (req: Request, res: Response): Promise<void> => {
     }
 
     const filters = validation.data;
-
     const whereClause: any = {};
 
+//filtros
     if (filters.id) {
         whereClause.id = filters.id;
     }
-    
-    // añadir mas lógica?
 
-    const movies = await prisma.movie.findMany({
-        where: whereClause
+    // 3. Ejecutar consulta en Prisma (Recuerda: la tabla es 'theater')
+    const cinemas = await prisma.theater.findMany({
+        where: whereClause,
+        // cd incluir relaciones si se pide conCatalog o withMovie??
     });
 
-
-    res.status(200).json(movies);
+    res.status(200).json(cinemas);
 
   } catch (error) {
     console.error(error);
---
+++ servidor/src/routes/routes.ts
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+import { getMovies } from '../controllers/movie.controller';
+import { getCinemas } from '../controllers/cinema.controller'; 
+
+const router = Router();
+
+router.post('/movies', getMovies);
+router.post('/cinemas', getCinemas); 
+
+export default router;
