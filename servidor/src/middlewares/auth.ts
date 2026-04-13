eb9a8fc959d89874d0029bbb0b172151d55a87dd
Diego <151045577+diegoofve@users.noreply.github.com>
2026-04-13 16:47:24 +0200

    terminar el front basandonos en el boiler

--- servidor/src/middlewares/auth.ts
+++ servidor/src/middlewares/auth.ts
@@ -4,7 +4,7 @@ import { Role } from "../../generated/prisma";
 export const authorize = (allowedRoles: Role[]) => {
     return (req: Request, res: Response, next: NextFunction): void => {
         // req.user es inyectado por Passport
-        const user = req.user as any;
+        const user = (req as any).user;
         if (!user || allowedRoles.includes(user.role)) {
             res.status(403).json({ error: 'No tienes permiso para acceder a este endpoint' });
         }
